502e8883d5c4bbc8f84a7702904535f8f26b7621
rui nogueira <ruinogueira@tuta.io>
2022-07-03 14:33:35 +0000

    STARTED web_server and aplication

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening on port 8080");
+    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
