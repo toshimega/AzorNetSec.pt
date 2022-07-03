4a8576e9d1f901fc62b21910789da5f4134fc42b
rui nogueira <ruinogueira@tuta.io>
2022-07-03 14:38:38 +0000

    Code Rub: Rename cargo project dependecy "azor_net_sec"

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,12 @@ async fn hello() -> impl Responder {
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()>{
     println!("Listening on port 8080");
-    HttpServer::new(|| App::new().route("/hello", web::get().to(hello)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new().route("/hello", web::get().to(hello))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
